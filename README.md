f33491541601aa1cb554238a37fa3440e7538e12
Gabinholindo <106189471+Gabinholindo@users.noreply.github.com>
2022-10-11 10:33:22 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Terceiro Trimestre
 
 ## Identificação
-Jesus de Maringá - Nr. 50
+Jesus - Nr. 50
 
 ## Conteúdo
-HTML, CSS, javaScript
+HTML, CSS e javaScript
--- estudo.js
+++ estudo.js
@@ -1,42 +1,45 @@
-// alert("Oi! Eu sou o Alert");
-// document.write("Estou na página.");
-// console.log("Eu estou no console.");
-let tabuada = 5;
-function alo(){
+//alert("");
+//console.log("");
+//document.write("");
 
-    let tabuada = document.getElementById("entrada").value;
+var tabuada = 8;
 
+function meChame(nome){
     document.write("<h1>Tabuada do " + tabuada + "</h1>");
-    document.write(tabuada + " x 1 = " + (tabuada*1)+"<br>");
-    document.write(tabuada + " x 2 = " + (tabuada*2)+"<br>");
-    document.write(tabuada + " x 3 = " + (tabuada*3)+"<br>");
-    document.write(tabuada + " x 4 = " + (tabuada*4)+"<br>");
-    document.write(tabuada + " x 5 = " + (tabuada*5)+"<br>");
-    document.write(tabuada + " x 6 = " + (tabuada*6)+"<br>");
-    document.write(tabuada + " x 7 = " + (tabuada*7)+"<br>");
-    document.write(tabuada + " x 8 = " + (tabuada*8)+"<br>");
-    document.write(tabuada + " x 9 = " + (tabuada*9)+"<br>");
-    document.write(tabuada + " x 10 = " + (tabuada*10));
+    document.write(tabuada + " x 1 = "+(tabuada*1)+"<br>");
+    document.write(tabuada + " x 2 = "+(tabuada*2)+"<br>");
+    document.write(tabuada + " x 3 = "+(tabuada*3)+"<br>");
+    document.write(tabuada + " x 4 = "+(tabuada*4)+"<br>");
+    document.write(tabuada + " x 5 = "+(tabuada*5)+"<br>");
+    document.write(tabuada + " x 6 = "+(tabuada*6)+"<br>");
+    document.write(tabuada + " x 7 = "+(tabuada*7)+"<br>");
+    document.write(tabuada + " x 8 = "+(tabuada*8)+"<br>");
+    document.write(tabuada + " x 9 = "+(tabuada*9)+"<br>");
+    document.write(tabuada + " x 10 = "+(tabuada*10)+"<br");
+    document.write("Feita por " + nome);
+    
 }
-// for(inicio, validaçao, manutençao)
+
 function escreva(){
-    for(var i=1; i <= 10; i++){
-        for(var j = 1; j <=10; j++){
-            document.write(i + " x " + j + " = " + (j*i)+"<br>");
-        }
-        document.write("<br>");
+    for(var i = 1; i < 11; i++){
+        document.write(tabuada + " x " + i + " = "+(tabuada*i)+"<br>");
     }
 }
 
 function quadrado(){
     for(var i = 2; i < 101; i++){
-        document.write("O quadrado de " + i + " é " + (i*i)+"<br>");
+        document.write("O Quadrado de " + i + " é " + (i*i)+ "<br>")
     }
 }
-
 function total(){
-    let valor = document.getElementById("valor").value;
-    let juros = document.getElementById("juros").value;
-    let resultado = (valor * (1+(juros/100)));
-    document.write("O total é " + resultado);
+    let valor = document.getElementById("val").value;
+    let juros = document.getElementById("ju").value;
+    let mes = document.getElementById("mes").value;
+    let resultado = 0;
+    for(let i = 1; i <= mes; i++){
+        resultado = valor * (1+(juros/100));
+        valor = resultado;
+    }
+    
+    document.write("O Resultado é " + resultado);
 }
